6d2c12abdc16f1efb1b799580260953350994ec0
niranjanaR15 <71608843+niranjanaR15@users.noreply.github.com>
2020-11-05 11:42:24 -0800

    initial commit

---
+++ lib/my-pipeline-stack.ts
@@ -0,0 +1,36 @@
+import { Stack, StackProps, Construct, SecretValue } from '@aws-cdk/core';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+
+export class MyPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      pipelineName: 'MyAppPipeline',
+      cloudAssemblyArtifact,
+
+      sourceAction: new codepipeline_actions.BitBucketSourceAction({
+        actionName: 'GitHub',
+        output: sourceArtifact,
+        connectionArn: 'arn:aws:codestar-connections:us-east-1:944732346921:connection/8954781f-c1f3-4040-aeb3-9385253b5a48',
+        owner: 'nr1597',
+        repo: 'cdk-test-pipeline'
+      }),
+
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+
+        // Use this if you need a build step (if you're not using ts-node
+        // or if you have TypeScript Lambdas that need to be compiled).
+        buildCommand: 'npm run build',
+      }),
+    });
+  }
+}
